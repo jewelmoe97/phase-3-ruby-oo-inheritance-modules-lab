e95f0cc22f2816331d164accd9bed7e50a5fd46b
jewel <snowdecember079@gmail.com>
2022-11-04 23:09:19 -0400

    pass

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
-
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   @@songs = []
 
   def initialize
@@ -18,13 +19,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
